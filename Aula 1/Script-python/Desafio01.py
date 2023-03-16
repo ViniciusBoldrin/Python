c9f7c43ccf7e8f820674d6358a3a99c4a9a4e268
Vinicius Boldrin <vinicius_boldrin@hotmail.com>
2023-03-16 10:37:29 -0400

    0.2

---
+++ Aula 1/Script-python/Desafio01.py
@@ -0,0 +1,12 @@
+nome=input('Qual seu nome?')
+print('Olá '+nome+'! Prazer em te conhecer !')
+
+dia=input('Qual o dia que voce nasceu?')
+mes=input('Qual mës voce nasceu?')
+ano=input('Qual ano voce nasceu?')
+print('Voce nasceu no dia '+dia+' de '+mes+' de '+ano+', estou correto Vivian?')
+
+n1=input('Insira o primeiro número ')
+n2=input('Insira o segundo número ')
+soma=int(n1)+int(n2)
+print('A soma dos números é:' soma)
---
+++ Aula 1/Script-python/teste01.py
@@ -0,0 +1,4 @@
+nome=input('Qual é seu nome?')
+peso=input('Qual é seu peso?')
+idade=input('Qual é sua idade?')
+print(nome, idade, peso)
