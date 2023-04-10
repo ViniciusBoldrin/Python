cacfde1e842c3602e5d6afd5c55fdd653fd16a9d
ViniciusBoldrin <126197366+ViniciusBoldrin@users.noreply.github.com>
2023-04-10 10:27:29 -0400

    Bug 02

--- Aulas/Aula09/Exercicio01.py
+++ Aulas/Aula09/Exercicio01.py
@@ -1,5 +1,11 @@
-nome = str(input('Escreva seu nome: '))
+nome = str(input('Escreva seu nome: ')).strip()
 print('Analisando...')
 
 print('Seu nome em maiusculo e: {}' .format(nome.upper()))
+print('Seu nome em minusculo e: {}' .format(nome.lower()))
+print('Seu nome tem {} letras!' .format(len(nome)-nome.count(' ')))
+print('Seu primeiro nome tem {} letras!' .format(nome.find(' ')))
+
+
+
 
---
+++ Aulas/Aula09/Exercicio02.py
@@ -0,0 +1,4 @@
+num = int(input('Digie um numero: '))
+n = str(num)
+
+print()
