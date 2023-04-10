8b0271e095bb3a8e72d7889a7f34649ab6cebb87
ViniciusBoldrin <126197366+ViniciusBoldrin@users.noreply.github.com>
2023-04-10 10:32:59 -0400

    Bug 03

--- Aulas/Aula09/Exercicio02.py
+++ Aulas/Aula09/Exercicio02.py
@@ -1,4 +1,9 @@
 num = int(input('Digie um numero: '))
 n = str(num)
 
-print()
+print('Analisando o numero {}' .format(num))
+
+print('Unidade: {}' .format(n[3]))
+print('Dezena: {}' .formtar(n[2]))
+print('Centena: {}' )
+print('Milhar: {}' )
